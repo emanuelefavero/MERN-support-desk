dc52dd438f66999cdc1716fc7fe1a027ba0323a7
emanuelefavero <emafaux89@gmail.com>
2022-06-12 07:28:21 +0200

    120. basic express server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('Server')
+require('dotenv').config()
+const express = require('express')
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    // ADD res.status(200) SENDS 200 CODE TO THIS GET REQUEST
+    res.status(200).json({ message: 'Welcome to the support desk API' })
+})
+
+app.listen(PORT, () => console.log(`server start on port ${PORT}`))
