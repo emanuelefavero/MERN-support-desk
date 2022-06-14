a72adbdb29dbe9def4c2448a4914307b8f9236c8
emanuelefavero <emafaux89@gmail.com>
2022-06-14 04:26:55 +0200

    124. register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,12 @@
 // add async functionality to express
 // '''npm i express-async-handler'''
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
 
+// User = mongoose.model('User', userSchema)
+const User = require('../models/userModel')
+
+// --- NOTE: Register User ---
 // @description Register a new user
 // @route       /api/users
 // @access      Public
@@ -13,9 +18,40 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error('Please include all fields')
     }
 
-    res.send('Register Route')
+    // find if user already exists
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        throw new Error('User already exists')
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10)
+    // this will hash the req.body.password:
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    })
+
+    // if user is created:
+    if (user) {
+        // show json with user data
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        })
+    } else {
+        res.status(400) // 400 Bad Request
+        throw new error('Invalid user data')
+    }
 })
 
+// --- NOTE: Login User ---
 // @description Login a user
 // @route       /api/users/login
 // @access      Public
