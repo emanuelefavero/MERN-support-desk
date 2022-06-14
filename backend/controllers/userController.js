de390fd3b1c86d4dd845528599865e4e45337f67
emanuelefavero <emafaux89@gmail.com>
2022-06-14 09:41:57 +0200

    126. protect routes and authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -81,6 +81,20 @@ const loginUser = asyncHandler(async (req, res) => {
     }
 })
 
+// --- NOTE: Get Current User ---
+// @description Get current user
+// @route       /api/users/me
+// @access      Private
+const getMe = asyncHandler(async (req, res) => {
+    const user = {
+        id: req.user._id,
+        name: req.user.name,
+        email: req.user.email,
+    }
+
+    res.status(200).json(user)
+})
+
 // generate token
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -92,4 +106,5 @@ const generateToken = (id) => {
 module.exports = {
     registerUser,
     loginUser,
+    getMe,
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser, loginUser } = require('../controllers/userController')
+const {
+    registerUser,
+    loginUser,
+    getMe,
+} = require('../controllers/userController')
+
+// NOTE: to protect a route, just add protect as a second argument in router
+const { protect } = require('../middleware/authMiddleware')
 
 // MAIN URL ROUTE /api/users/ (SEE server.js)
 router.post('/', registerUser) // /api/users/
 router.post('/login', loginUser) // /api/users/login
+router.get('/me', protect, getMe) // /api/users/me NOTE: PROTECTED
 
 // EXPORT router
 module.exports = router
